eab6100f60c35a5575f25662900655df8072b461
Dev-Lmm <lmm300997@gmail.com>
2023-03-22 14:25:12 -0600

    first commit

---
+++ src/main/java/com/devlmm/spring/apirest/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.devlmm.spring.apirest.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+
+    //private final RoleService roleService;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        //String[] roles = roleService.getAllRolesAsString();
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/dev-lmm/auth/**")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "api/clientes/{id}")
+                .hasAnyRole("USER", "ADMIN")
+                //.hasAnyRole(roles)
+                .requestMatchers(HttpMethod.POST, "api/clientes/upload")
+                .hasAnyRole("USER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "api/clientes")
+                .hasRole("ADMIN")
+                .requestMatchers( "api/clientes/**")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/dev-lmm/auth/logout")
+                .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+
+        return http.build();
+    }
+}
